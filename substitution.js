efce42b93aada18a70526472e01b57d20cbb9b08
Tangerine Cat <kaineer@gmail.com>
2011-05-30 07:10:25 +0000

    W/o variable

--- substitution.js
+++ substitution.js
@@ -1,7 +1,6 @@
 var foo = "foo";
-var bar = "bar";
 
-console.log(foo);        // -> "foo"
+console.log(foo); // -> "foo"
 (function(foo){
-  console.log(foo);    // -> "bar"
-})(bar);
+  console.log(foo); // -> "bar"
+})("bar");
